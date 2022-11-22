aae0b3fd5acc037540cf3d0f8c9b169dc099326e
MrDuking <huuduc122003@gmail.com>
2022-11-22 13:05:37 +0700

    Hoan thien database thong tin khach hang

--- cashout_page.php
+++ cashout_page.php
@@ -6,14 +6,7 @@ if(isset($_POST["submit"])){
   $DiaChi=$_POST["adr"];
   $ThanhPho=$_POST["city"];
   // $Quan = isset($_POST['state']) ? $_POST['state'] : false;
-  $Quan = isset($_POST['state']) ? $_POST['state'] : false;
-  if ($Quan) {
-     echo htmlentities($_POST['state'], ENT_QUOTES, "UTF-8");
-  } else {
-    echo "task option is required";
-    exit; 
-  }
-  echo $Quan;
+  $Quan = $_POST['state'];
   if(isset($Hoten)&& isset($Email)&& isset($DiaChi)&& isset($ThanhPho) && isset($Quan)){;
 
     $data = [
@@ -96,26 +89,26 @@ if(isset($_POST["submit"])){
               <div class="col-50">
                 <label for="state">Quận</label>
                 <select type="" id="state" name="state">
-                    <option value="1">Quận 1</option>
-                    <option value="2">Quận 2</option>
-                    <option value="3">Quận 3</option>
-                    <option value="4">Quận 4</option>
-                    <option value="5">Quận 5</option>
-                    <option value="6">Quận 6</option>
-                    <option value="7">Quận 7</option>
-                    <option value="8">Quận 8</option>
-                    <option value="9">Quận 9</option>
-                    <option value="10">Quận 10</option>
-                    <option value="11">Quận 11</option>
-                    <option value="12">Quận 12</option>
-                    <option value="13">Quận Bình Tân</option>
-                    <option value="14">Quận Bình Thạnh</option>
-                    <option value="15">Quận Tân Bình</option>
-                    <option value="16">Quận Tân Phú</option>
-                    <option value="17">Quận Thủ Đức</option>                    
-                    <option value="18">Quận Phú Nhuận</option>
-                    <option value="19">Quận Hóc Môn</option>
-                    <option value="20">Quận Gò Vấp</option>                    
+                    <option value="Quận 1">Quận 1</option>
+                    <option value="Quận 2">Quận 2</option>
+                    <option value="Quận 3">Quận 3</option>
+                    <option value="Quận 4">Quận 4</option>
+                    <option value="Quận 5">Quận 5</option>
+                    <option value="Quận 6">Quận 6</option>
+                    <option value="Quận 7">Quận 7</option>
+                    <option value="Quận 8">Quận 8</option>
+                    <option value="Quận 9">Quận 9</option>
+                    <option value="Quận 10">Quận 10</option>
+                    <option value="Quận 11">Quận 11</option>
+                    <option value="Quận 12">Quận 12</option>
+                    <option value="Quận Bình Tân">Quận Bình Tân</option>
+                    <option value="Quận Bình Thạnh">Quận Bình Thạnh</option>
+                    <option value="Quận Tân Bình">Quận Tân Bình</option>
+                    <option value="Quận Tân Phú">Quận Tân Phú</option>
+                    <option value="Quận Thủ Đức">Quận Thủ Đức</option>                    
+                    <option value="Quận Phú Nhuận">Quận Phú Nhuận</option>
+                    <option value="Quận Hóc Môn<">Quận Hóc Môn</option>
+                    <option value="Quận Gò Vấp">Quận Gò Vấp</option>                    
                 </select>
             </div>
             </div>
